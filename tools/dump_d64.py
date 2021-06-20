b472154b39741315911e000bf10f17dd8d87ff72
Ricardo Quesada <ricardoquesada@gmail.com>
2021-06-20 08:04:24 -0700

    tools: dump c64 wait for output
    wait for output to have a synchronized output

--- tools/dump_d64.py
+++ tools/dump_d64.py
@@ -6,5 +6,6 @@ import sys
 # Ignore first argument which is "self"
 for name in sys.argv[1:]:
     print(name)
-    subprocess.run(("c1541", name, "-list"))
+    out = subprocess.check_output(("c1541", name, "-list"))
+    print(out.decode("utf-8"))
     print("\n")
